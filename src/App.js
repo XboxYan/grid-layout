820be5c78ee1ef763c0cccfc620b6a7a5a8cfe4b
XboxYan <yanwenbin1991@live.com>
2019-04-24 18:16:00 +0800

    init

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import styles from './App.module.css';
+
+import React from 'react';
+import { Layout,Icon } from 'antd';
+
+const { Header, Footer, Sider, Content } = Layout;
+
+
+
+const App = () => (
+	<Layout>
+		<Sider>
+			<div className={styles.logo}><Icon type="layout" /><span>Grid Layout</span></div>
+		</Sider>
+		<Layout>
+			<Header className={styles.header}>Grid Layout</Header>
+			<Content>Content</Content>
+			<Footer>Footer</Footer>
+		</Layout>
+	</Layout>
+)
+
+export default App;
